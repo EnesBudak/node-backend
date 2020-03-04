08cb2ebb17e52d3468e20236e8066a645246f33f
EnesBudak <45215087+EnesBudak@users.noreply.github.com>
2020-03-04 16:23:24 +0300

    v0.1
    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// import routes
+const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
+
+dotenv.config();
+
+// connect to DB
+mongoose.connect(
+  process.env.DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    try {
+      console.log("db connected");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+app.use(express.json());
+
+// route middlewares
+app.use("/api/user", authRoute);
+app.use("/api/posts", postsRoute);
+app.listen(3000, () => {
+  console.log("Server up and running");
+});
